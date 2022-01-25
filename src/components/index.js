a0523745c36cb5e593ede52b068452e828032a32
Mr_Jerry <jerrykano@gmail.com>
2022-01-24 23:54:07 -0800

    Login/TOKEN

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as NavBar } from './NavBar';
+export { default as AllPosts } from './AllPosts';
+export { default as Login} from './Login';
+export { default as LoginForm} from './Register';
+export { default as SinglePostElement } from './SinglePostElement'
+export { default as PostForm } from './PostForm'
+export { default as App} from '../App'
+export { default as TodoList} from './todoList'
+
